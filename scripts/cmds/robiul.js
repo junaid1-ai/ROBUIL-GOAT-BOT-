115c3b9c74bc5358981a6815a1c98f823e022dcd
junaid1-ai <arjunaid510@gmail.com>
2026-03-17 17:40:56 +0600

    Add scripts/cmds/robiul.js via Zip Uploader

---
+++ scripts/cmds/robiul.js
@@ -0,0 +1,22 @@
+ module.exports = {
+ config: {
+	 name: "robiul",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "robiul") {
+ return message.reply({
+ body: "     「রবিউল বস এখন কাজে বিজি আছেন যা বলার আমাকে বলতে পারেন🤗😚」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/SoUK0cq.mp4")
+ });
+ }
+ }
+}
